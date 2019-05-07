24b96a05e3adbd5958ea5dba57da06cd51ca0d92
Luong Vo <vo.tran.thanh.luong@gmail.com>
2019-05-07 10:59:19 +0700

    Add some prototype

--- lib/eh/mailer.rb
+++ lib/eh/mailer.rb
@@ -1,6 +1,23 @@
-require "eh/mailer/version"
+require 'eh/mailer/version'
+require 'eh/mailer/railtie' if defined? Rails
 
 module Eh
   module Mailer
+    class Delivery
+      attr_accessor :message
+
+      def initialize(**params)
+        @params = params
+      end
+
+      def deliver!(mail)
+        puts "from #{mail.from}"
+        puts "to #{mail.to}"
+
+        mail.parts.each do |m|
+          puts "Content: #{m.body}"
+        end
+      end
+    end
   end
 end
