ae6eaca6f9b798a161c0c3393709465e29fe9d07
Luong Vo <vo.tran.thanh.luong@gmail.com>
2019-05-07 17:21:13 +0700

    Add working kafka

--- eh-mailer.gemspec
+++ eh-mailer.gemspec
@@ -35,7 +35,9 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
+  spec.add_dependency "ruby-kafka", "~> 0.7.6"
   spec.add_development_dependency "bundler", "~> 2.0"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_development_dependency "byebug"
 end
--- lib/eh/mailer.rb
+++ lib/eh/mailer.rb
@@ -1,21 +1,30 @@
 require 'eh/mailer/version'
 require 'eh/mailer/railtie' if defined? Rails
+require 'json'
+require 'kafka'
+require_relative './kafka_worker'
 
 module Eh
   module Mailer
-    class Delivery
+    class DeliveryMethod
+      MAILER_TOPIC_NAME = 'EmploymentHero.Emails'.freeze
       attr_accessor :message
+
+      def kafka_client
+        @kafka_client ||= KafkaWorker.new(@params[:kafka_client])
+      end
 
       def initialize(**params)
         @params = params
       end
 
       def deliver!(mail)
-        puts "from #{mail.from}"
-        puts "to #{mail.to}"
-
-        mail.parts.each do |m|
-          puts "Content: #{m.body}"
+        mail_data = mail.to_json
+        if @params[:kafka_publish_method]
+          package_data = { message: mail_data, topic: MAILER_TOPIC_NAME }
+          kafka_client.send(@params[:kafka_publish_method], package_data)
+        else
+          kafka_client._publish_message(mail_data, MAILER_TOPIC_NAME)
         end
       end
     end
