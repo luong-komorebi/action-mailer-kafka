ae419d3f96fb9d7a72aba69482f43410f6ef4c6d
Luong Vo <vo.tran.thanh.luong@gmail.com>
2019-05-15 13:37:24 +0700

    Update publish scripts

--- scripts/publish.rb
+++ scripts/publish.rb
@@ -3,4 +3,4 @@ require 'json'
 manifest_path = File.expand_path('../../app.json', __FILE__)
 version = JSON.parse(File.read(manifest_path))['version']
 
-exec("gem build eh_monitoring.gemspec && curl -F package=@eh_monitoring-#{version}.gem https://#{ENV['GEMFURY_TOKEN']}@push.fury.io/#{ENV['GEMFURY_PACKAGE']}/")
+exec("gem build eh-mailer.gemspec && curl -F package=@eh-mailer-#{version}.gem https://#{ENV['GEMFURY_TOKEN']}@push.fury.io/#{ENV['GEMFURY_PACKAGE']}/")
--- scripts/publish_development.rb
+++ scripts/publish_development.rb
@@ -5,4 +5,4 @@ version = JSON.parse(File.read(manifest_path))['version']
 
 abort('Development Gem must have .dev in version') unless version.include?('dev')
 
-exec("gem build eh_monitoring.gemspec && curl -F package=@eh_monitoring-#{version}.gem https://#{ENV['GEMFURY_TOKEN']}@push.fury.io/#{ENV['GEMFURY_PACKAGE']}/")
+exec("gem build eh-mailer.gemspec && curl -F package=@eh-mailer-#{version}.gem https://#{ENV['GEMFURY_TOKEN']}@push.fury.io/#{ENV['GEMFURY_PACKAGE']}/")
