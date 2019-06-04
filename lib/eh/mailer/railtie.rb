5cdfe74153fc20b60839119e5bf2e9023f874d98
Luong Vo <vo.tran.thanh.luong@gmail.com>
2019-06-04 15:55:12 +0700

    Remove files for codereview

--- lib/eh/mailer/railtie.rb
+++
@@ -1,9 +0,0 @@
-module Eh
-  module Mailer
-    class Railtie < Rails::Railtie
-      initializer 'eh_mailer.add_delivery_method', before: 'action_mailer.set_configs' do
-        ActionMailer::Base.add_delivery_method :eh_mailer, Eh::Mailer::DeliveryMethod
-      end
-    end
-  end
-end
