a5705cfbac9b1e351972482407486ef678fa630f
Luong Vo <vo.tran.thanh.luong@gmail.com>
2019-06-10 10:03:29 +0700

    Delete development version file

--- lib/eh/mailer/dev_version.rb
+++
@@ -1,5 +0,0 @@
-module Eh
-  module Mailer
-    DEV_VERSION = '0.1.2.dev'.freeze
-  end
-end
--- scripts/publish.rb
+++ scripts/publish.rb
@@ -2,4 +2,4 @@ lib = File.expand_path('../lib', __dir__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'eh/mailer/version'
 
-exec("gem build eh-mailer.gemspec && curl -F package=@eh-mailer-#{Eh::Mailer::DEV_VERSION}.gem https://#{ENV['GEMFURY_TOKEN']}@push.fury.io/#{ENV['GEMFURY_PACKAGE']}/")
+exec("gem build eh-mailer.gemspec && curl -F package=@eh-mailer-#{Eh::Mailer::VERSION}.gem https://#{ENV['GEMFURY_TOKEN']}@push.fury.io/#{ENV['GEMFURY_PACKAGE']}/")
